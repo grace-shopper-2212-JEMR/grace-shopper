2f646b73abe0ccae06ebb5b5fd5ee52b6f9e877a
Joe Del Balzo <jdelbalzo99@gmail.com>
2023-04-26 13:16:37 -0400

    product page v1

--- src/Components/DrinkProductPage.js
+++ src/Components/DrinkProductPage.js
@@ -4,9 +4,13 @@ import { useSelector, useDispatch } from "react-redux";
 
 const DrinkProductPage = () =>{
   return (
+    <>
+    
     <div>
       <h2>This is the page for a SINGLE drink product. We'll put coffees and teas that people can order here.</h2>
     </div>
+    </>
+
   )
 }
 
