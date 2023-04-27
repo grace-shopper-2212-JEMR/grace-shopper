579682689679377c638aa6180aabb749a5eacd2b
Mike Norris <mnorris6@elon.edu>
2023-04-26 21:15:27 -0400

    done with about

---
+++ src/Components/About.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const About = () => {
+  return (
+    <div>
+       <Typography>
+        Let me tell you, there's nothing quite like a good cup of coffee to get the brain humming and the ideas flowing. And when you add technology to the mix, well, you've got something truly special. As shown by the most current studies, the human race needs a prescription for coffee! That's where we come in.
+
+At our coffee company Script For Java, we're not just brewing up a tasty cup of joe, oh no. We're combining the best of two worlds, the world of coffee and the world of tech, to create something truly remarkable.
+
+Every cup of coffee we serve is made with the finest beans, roasted to perfection, and brewed with care. And every customer who walks through our door is treated like a valued member of the community.
+
+But that's not all, my friend. Our coffee company is more than just a place to grab a cup of coffee. It's a hub for tech enthusiasts, a place where they can come together to exchange ideas, discuss the latest trends, and work on their projects.
+
+So if you're looking for a place to fuel your mind and body, look no further than Script for Java. With every sip of our delicious brew, you'll be inspired to tackle the challenges of the day, and with every visit, you'll be part of a vibrant community of like-minded individuals.
+
+In short, our company is the perfect blend of coffee and technology, and I can't think of a better way to start your day.
+</Typography>
+    </div>
+  )
+}
+
+export default About
--- src/Components/SubNavMerch.js
+++ src/Components/SubNavMerch.js
@@ -71,6 +71,66 @@ function SubNavMerch() {
             Mugs
           </Typography>
 
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/merch/shirts"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex'},
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+              justifyContent: 'center'
+            }}
+          >
+            Shirts
+          </Typography>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/merch/hats"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex'},
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+              justifyContent: 'center'
+            }}
+          >
+            Hats
+          </Typography>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/merch/mugs"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex'},
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+              justifyContent: 'center'
+            }}
+          >
+            Mugs
+          </Typography>
+
         </Toolbar>
       </Container>
     </AppBar>
