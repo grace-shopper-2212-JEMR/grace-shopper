0c4c5dc589ff6a429a08f912f6fb40e77c40823e
Mike Norris <mnorris6@elon.edu>
2023-04-27 01:24:19 -0400

    added logout page and more routing

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateAuth } from '../store';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 
 const Account = ()=> {
@@ -29,20 +29,38 @@ const Account = ()=> {
     dispatch(updateAuth({ firstName, lastName, address, email, phone }));
   };
   return (
-    <form onSubmit={ _update }>
-      <div style={{ marginBottom: 8 }}/>
-      <TextField label="First Name" variant="outlined" value={ firstName } onChange={ev => setFirstName(ev.target.value)}/>
-      <div style={{ marginBottom: 8 }}/>
-      <TextField label="Last Name" variant="outlined" value={ lastName } onChange={ev => setLastName(ev.target.value)}/>
-      <div style={{ marginBottom: 8 }}/>
-      <TextField label="Address" variant="outlined" value={ address } onChange={ev => setAddress(ev.target.value)}/>
-      <div style={{ marginBottom: 8 }}/>
-      <TextField label="E-mail" variant="outlined"  value={ email } onChange={ev => setEmail(ev.target.value)}/>
-      <div style={{ marginBottom: 8 }}/>
-      <TextField label="Phone" variant="outlined" value={ phone } onChange={ev => setPhone(ev.target.value)}/>
+    <div>
+      {
+        auth.id ? (
+            <div>
+                <h1>Enter Account Info Here</h1>
+                <form onSubmit={ _update }>
+                  <div style={{ marginBottom: 8 }}/>
+                  <TextField label="First Name" variant="outlined" value={ firstName } onChange={ev => setFirstName(ev.target.value)}/>
+                  <div style={{ marginBottom: 8 }}/>
+                  <TextField label="Last Name" variant="outlined" value={ lastName } onChange={ev => setLastName(ev.target.value)}/>
+                  <div style={{ marginBottom: 8 }}/>
+                  <TextField label="Address" variant="outlined" value={ address } onChange={ev => setAddress(ev.target.value)}/>
+                  <div style={{ marginBottom: 8 }}/>
+                  <TextField label="E-mail" variant="outlined"  value={ email } onChange={ev => setEmail(ev.target.value)}/>
+                  <div style={{ marginBottom: 8 }}/>
+                  <TextField label="Phone" variant="outlined" value={ phone } onChange={ev => setPhone(ev.target.value)}/>
       
-      <button disabled={ firstName === auth.firstName && lastName === auth.lastName && address === auth.address && email === auth.email && phone === auth.phone}>Update Profile</button>
-    </form>
+                  <button disabled={ firstName === auth.firstName && lastName === auth.lastName && address === auth.address && email === auth.email && phone === auth.phone}>Update Profile</button>
+                </form>
+            </div>
+        
+        )  : (
+            <div>
+                <h1>Can't Update If You're Not Logged In!</h1>
+                <div>
+                    <Link to={`/register`}>Register Here</Link> or <Link to='/login'> Login </Link>
+                </div>
+            </div>
+          )
+        } 
+     
+    </div>
   );
 };
 
