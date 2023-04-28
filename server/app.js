f3c6a1b0525e2e7078ea71e5f96342dbd592f503
Joe Del Balzo <jdelbalzo99@gmail.com>
2023-04-27 20:05:27 -0400

    merches page start

--- server/app.js
+++ server/app.js
@@ -11,5 +11,6 @@ app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../static/index.htm
 app.use('/api/auth', require('./api/auth'));
 app.use('/api/orders', require('./api/orders'));
 app.use('/api/drinks', require('./api/drinks'));
+app.use('/api/merches', require('./api/merches'));
 
 module.exports = app;
