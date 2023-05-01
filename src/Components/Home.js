01ad9fcbb92bc3263b102ac885fb75b0752cdbc4
Rebecca Cherry <120757679+rebeccacherry@users.noreply.github.com>
2023-05-01 00:57:07 -0400

    background image added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,17 +3,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../store';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom'
-import backgroundImg from 'src/images/coffee_cup_illustration_blue.jpeg'
+
 const Home = ()=> {
   const { auth } = useSelector(state => state);
   const dispatch = useDispatch();
   return (
+    <div className='background' style={{ backgroundImage: "url(static/images/coffee_cup_illustration_blue.jpeg)" }}>
     <div>
       
        {
-
         (!auth.id) ? 
-
      (
       <div>
 
@@ -25,33 +24,20 @@ const Home = ()=> {
         {/* <Button variant='outlined' ><Link to={`/register`}>Register Here</Link></Button> or <Button variant='outlined'> <Link to='/login'> Login </Link></Button> */}
       </div>
   </div>        
-
-        console.log(auth.id)
-        (
-            <div>
-                <h1>Welcome {auth.username}</h1>
-                <div>
-                  <div>
-                    Please browse amongst our delicious selection of drinks or our fashionable merch.
-                  </div>
-                  <Button variant="contained" onClick={()=> dispatch(logout())}>Logout</Button>
-                </div>
-            </div>
-        
-
         )  : (
           <div>
-            <img src={backgroundImg}/>
+           
           <h1>Welcome {auth.username}</h1>
           <div>
             <div>
               Please browse amongst our delicious selection of drinks or our fashionable merch.
             </div>
           </div>
-        
-      </div>
+        </div>
+    
         )
         } 
+          </div>
     </div>
   );
 };
