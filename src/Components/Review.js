9645637664e530473355cdf50d0eb70cc9ceb146
Rebecca Cherry <120757679+rebeccacherry@users.noreply.github.com>
2023-05-03 16:26:11 -0400

    Update Review.js

---
+++ src/Components/Review.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import { Box, Button, IconButton, TableCell, TableRow } from '@mui/material';
+import { Edit, Delete } from '@mui/icons-material';
+
+const ReviewItem = ({ review, deleteReview }) => (
+  <TableRow>
+    <TableCell>{review.id}</TableCell>
+    <TableCell>{review.rating}</TableCell>
+    <TableCell>{review.subject}</TableCell>
+    <TableCell>{review.product.name}</TableCell>
+    <TableCell>{review.userId}</TableCell>
+    <TableCell>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <IconButton component={Link} to={`/review/${review.id}`}>
+          <Edit />
+        </IconButton>
+        <IconButton onClick={(event) => deleteReview(event, review)}>
+          <Delete />
+        </IconButton>
+      </Box>
+    </TableCell>
+  </TableRow>
+);
+
+export default ReviewItem;
