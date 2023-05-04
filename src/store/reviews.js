5739d88f9831e149d2d6d1383da7d17b64e8b60e
Joe Del Balzo <jdelbalzo99@gmail.com>
2023-05-03 20:56:34 -0400

    admin login

--- src/store/reviews.js
+++ src/store/reviews.js
@@ -45,7 +45,6 @@ const reviews = (state = [], action)=> {
   export const createReview = (review)=> {
     return async(dispatch)=> {
       const response = await axios.post(`api/reviews`, review, productId);
-      console.log(response, 'response')
       dispatch({type: 'CREATE_REVIEW', review: response.data})
     };
   };
